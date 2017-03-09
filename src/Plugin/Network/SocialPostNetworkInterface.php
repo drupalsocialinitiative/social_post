17390d7b2d3d0f5919a411353cb5223d650e1026
Getulio Sánchez <valentin2507@gmail.com>
2017-03-09 10:57:18 -0300

    Issue #2858944: Update post method documentation for returning boolean in successful or failed posting

--- src/Plugin/Network/SocialPostNetworkInterface.php
+++ src/Plugin/Network/SocialPostNetworkInterface.php
@@ -13,6 +13,10 @@ interface SocialPostNetworkInterface extends NetworkInterface {
    * Execute the posting action.
    *
    * Uses the underlying SDK library to publish to the social network.
+   *
+   * @return bool
+   *   True if the post action was done properly
+   *   False otherwise
    */
   public function post();
 
