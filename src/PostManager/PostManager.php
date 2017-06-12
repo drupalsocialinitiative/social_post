936ad962aaf1b435d941624b27ccf251cabf7dd8
Himanshu Dixit <hudixt@gmail.com>
2017-06-13 00:25:14 +0530

    Update PostManager.php

--- src/PostManager/PostManager.php
+++ src/PostManager/PostManager.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\social_post\PostManager;
 
-use Drupal\social_api\OAuth2Manager;
+use Drupal\social_api\OAuth2Manager as BaseOAuth2Manager;
 
 /**
  * Defines a basic PostManager.
  *
  * @package Drupal\social_post
  */
-abstract class PostManager extends OAuth2Manager\OAuth2Manager implements PostManagerInterface {
+abstract class PostManager extends BaseOAuth2Manager implements PostManagerInterface {
 
   // Do Nothing.
 }
