71174dba5e728cda3a94e3c1f180dc78048886fd
Himanshu Dixit <hudixt@gmail.com>
2017-06-13 19:57:09 +0530

    Create PostManager and PostManagerInterface (#2)

---
+++ src/PostManager/PostManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\social_post\PostManager;
+
+use Drupal\social_api\AuthManager\OAuth2Manager;
+
+/**
+ * Defines a basic PostManager.
+ *
+ * @package Drupal\social_post
+ */
+abstract class PostManager extends OAuth2Manager implements PostManagerInterface {
+
+  // Do Nothing.
+}
---
+++ src/PostManager/PostManagerInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\social_post\PostManager;
+
+use Drupal\social_api\AuthManager\OAuth2ManagerInterface
+
+/**
+ * Defines an PostManager Interface.
+ *
+ * @package Drupal\social_post\PostManager
+ */
+interface PostManagerInterface extends OAuth2ManagerInterface {
+
+  // Do nothing.
+}
