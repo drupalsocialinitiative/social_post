7a015b93cad12c1802bdc49f7b91eb9ad61bc0e3
neerajp99 <neerajp1999@gmail.com>
2019-06-11 05:03:48 +0530

    Add tests for class UserAccessController

---
+++ tests/src/Unit/UserAccessControlHandlerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Drupal\social_post\UserAccessControlHandler;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Defines UserAccessControlerHandler class.
+ *
+ * @Annotation
+ */
+class UserAccessControlHandlerTest extends UnitTestCase {
+
+  /**
+   * Define __construct function.
+   */
+  public function __construct() {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Tests for class UserAccessControlHandler.
+   */
+  public function testUserAccessControlHandler() {
+    $entity = $this->createMock(EntityInterface::class);
+    $account = $this->createMock(AccountInterface::class);
+    $entity_type = $this->createMock(EntityTypeInterface::class);
+    $collection = $this->getMockBuilder(EntityAccessControlHandler::class)
+                                     ->setConstructorArgs(array($entity_type))
+                                     ->getMock();
+    $userAccessControlHandler = $this->getMockBuilder(UserAccessControlHandler::class)
+                                     ->setConstructorArgs(array($entity_type))
+                                     ->getMock();
+
+    // $userAccessControlHandler->checkAccess($entity, 'view', $account);
+    // $reflection = new ReflectionClass(UserAccessControlHandler::class);
+    // $method = $reflection->getMethod('checkAccess');
+    // $method->setAccessible(true);
+    // return $method;
+    $this->assertTrue(
+          method_exists($userAccessControlHandler, 'checkAccess'),
+            'ControllerBase does not implements checkAccess function/method'
+    );
+    $this->assertTrue(
+          method_exists($userAccessControlHandler, 'checkCreateAccess'),
+            'ControllerBase does not implements checkCreateAccess function/method'
+    );
+  }
+
+}
