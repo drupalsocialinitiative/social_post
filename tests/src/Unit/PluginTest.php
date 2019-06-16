e9c2521e46cad2bb37fc035a9d234557b2bb4b62
neerajp99 <neerajp1999@gmail.com>
2019-06-16 22:20:17 +0530

    Edit tests for Plugin class

--- tests/src/Unit/PluginTest.php
+++ tests/src/Unit/PluginTest.php
@@ -12,23 +12,10 @@ use Drupal\Tests\UnitTestCase;
 class NetworkTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Tests for class Network.
    */
   public function testSocialPostNetworkInterface() {
+
     $socialPostNetworkInterface = $this->getMockBuilder(SocialPostNetworkInterface::class)
                                        ->getMock();
     $this->assertTrue(
