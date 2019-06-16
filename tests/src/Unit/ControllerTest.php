2c8efda39b20ee206c225f797ffb4b1e58741481
neerajp99 <neerajp1999@gmail.com>
2019-06-16 21:50:32 +0530

    Edit tests for Controller

--- tests/src/Unit/ControllerTest.php
+++ tests/src/Unit/ControllerTest.php
@@ -13,33 +13,23 @@ use Drupal\Tests\UnitTestCase;
 class ControllerTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Tests for class Network.
    */
   public function testControllerBase() {
     $controllerBase = $this->getMockBuilder(ControllerBase::class)
                            ->getMock();
+
     $listBuilder = $this->createMock(SocialPostListBuilder::class);
+
     $controllerBase->method('buildList')
                    ->with('facebook')
                    ->will($this->returnValue($listBuilder->render()));
+
     $this->assertTrue(
           method_exists($controllerBase, 'buildList'),
             'ControllerBase does not implements buildList function/method'
     );
+
     $this->assertEquals($listBuilder->render(), $controllerBase->buildList('facebook'));
   }
 
